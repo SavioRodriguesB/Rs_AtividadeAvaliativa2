15da81b6f2f6b4f16540416a4c4030e3b968ef79
SavioRodrigues <saviobrjf28@gmail.com>
2021-06-11 08:26:38 -0300

    Criação do projeto

---
+++ src/main/java/br/com/uniacademia/rs_avaliativa2/service/ServiceFormatacao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.uniacademia.rs_avaliativa2.service;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author savio
+ */
+
+@Path("/formatar")
+public class ServiceFormatacao {
+    
+}
